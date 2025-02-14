7bcf678f9632df2bc6e6ee6511df03447648c6a2
Wel15 <ffw150@163.com>
2025-02-14 14:14:18 +0800

    v4 21

--- sp1-tests/test-bn254/script/src/main.rs
+++ sp1-tests/test-bn254/script/src/main.rs
@@ -1,7 +1,8 @@
 use sp1_prover::utils::get_cycles;
+use sp1_sdk::include_elf;
 use sp1_sdk::{utils, SP1Stdin};
 
-const ELF: &[u8] = include_elf!("../poseidon-bn254-test-program");
+const ELF: &[u8] = include_elf!("poseidon-bn254-test-program");
 
 fn main() {
     utils::setup_logger();
