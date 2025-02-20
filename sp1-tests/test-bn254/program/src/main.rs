583f47b8383eb2fc3bd8c14df8fcc2029172170f
anylots <22675649+anylots@users.noreply.github.com>
2025-02-20 16:31:04 +0800

    add poseidon hash prove

--- sp1-tests/test-bn254/program/src/main.rs
+++ sp1-tests/test-bn254/program/src/main.rs
@@ -1,18 +1,10 @@
 #![no_main]
 
 sp1_zkvm::entrypoint!(main);
-
-use itertools::iproduct;
-use poseidon_bn254::{hash_code, hash_msg, hash_with_domain, Fr};
-use std::array;
+use poseidon_bn254::{hash_with_domain, Fr};
 
 fn main() {
     // test hash_with_domain
-    println!("cycle-tracker-start: hash_with_domain(&[Fr::zero(), Fr::zero()], Fr::zero())");
-    let result1 = hash_with_domain(&[Fr::zero(), Fr::zero()], Fr::zero());
-    println!("Result: {:?}", result1);
-    println!("cycle-tracker-end: hash_with_domain(&[Fr::zero(), Fr::zero()], Fr::zero())");
-
     println!(
         "cycle-tracker-start: hash_with_domain(&[Fr::from(1u64), Fr::from(2u64)], Fr::from(3u64))"
     );
@@ -21,43 +13,4 @@ fn main() {
     println!(
         "cycle-tracker-end: hash_with_domain(&[Fr::from(1u64), Fr::from(2u64)], Fr::from(3u64))"
     );
-
-    // test hash_msg
-    let msgs = [
-        &array::from_fn::<_, 1, _>(|i| Fr::from(i as u64))[..],
-        &array::from_fn::<_, 10, _>(|i| Fr::from(i as u64))[..],
-        &array::from_fn::<_, 11, _>(|i| Fr::from(i as u64))[..],
-    ];
-
-    let cap = [None, Some(100)];
-
-    for (msg, cap) in iproduct!(msgs.iter(), cap.iter()) {
-        let tag = format!("hash_msg({}, {:?})", msg.len(), cap);
-        println!("{}", format!("cycle-tracker-start: {tag}"));
-        let result = hash_msg(msg, *cap);
-        println!("Result: {:?}", result);
-        println!("{}", format!("cycle-tracker-end: {tag}"));
-    }
-
-    // test hash_code
-    let codes = [
-        &[],
-        &array::from_fn::<_, 1, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 128, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 256, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 512, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 1024, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 2048, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 4096, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 16384, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 24576, _>(|i| i as u8)[..],
-    ];
-
-    for code in codes.iter() {
-        let tag = format!("hash_code({})", code.len());
-        println!("{}", format!("cycle-tracker-start: {tag}"));
-        let result = hash_code(code);
-        println!("Result: {:?}", result);
-        println!("{}", format!("cycle-tracker-end: {tag}"));
-    }
 }
--- sp1-tests/test-bn254/script/src/main.rs
+++ sp1-tests/test-bn254/script/src/main.rs
@@ -1,11 +1,16 @@
 use sp1_prover::utils::get_cycles;
-use sp1_sdk::include_elf;
-use sp1_sdk::{utils, SP1Stdin};
+use sp1_sdk::{utils, ProverClient, SP1Stdin};
 
-const ELF: &[u8] = include_elf!("poseidon-bn254-test-program");
+const ELF: &[u8] = include_bytes!("../../elf/riscv32im-succinct-zkvm-elf");
 
 fn main() {
     utils::setup_logger();
-    let cycles = get_cycles(ELF, &SP1Stdin::default());
-    println!("final cycles: {cycles}");
+    let client = ProverClient::from_env();
+    let (pk, vk) = client.setup(ELF);
+    let mut proof = client
+        .prove(&pk, &SP1Stdin::default())
+        .compressed()
+        .run()
+        .unwrap();
+    println!("final cycles: {:?}", proof.sp1_version.len());
 }
